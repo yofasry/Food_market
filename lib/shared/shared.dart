c26ef450ab4704a2b1c48d4d36accaac6a814c07
yofasry <yofasry@gmail.com>
2025-01-05 19:41:11 +0700

    1

---
+++ lib/shared/shared.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'package:food_market/ui/pages/pages.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:supercharged/supercharged.dart';
+
+part 'theme.dart';
